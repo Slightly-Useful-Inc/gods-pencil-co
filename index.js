213762a98cf4734ed9df1175825c789f298f46bc
SkilzMastr <64101555+SkilzMastr@users.noreply.github.com>
2020-12-08 17:30:58 -0500

    Added base of api

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.static('./public'));
 
-app.request('/api/prices', (req, res)=>{
+app.get('/api/prices', (req, res)=>{
     res.json("./public/api/prices.json")
 });
 
