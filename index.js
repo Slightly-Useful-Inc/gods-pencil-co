5a7f48dca8e369a8aeb9101b66c75d8f53dac1c7
SkilzMastr <64101555+SkilzMastr@users.noreply.github.com>
2020-12-09 17:36:24 -0500

    Made Server.js

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
-const express = require("express");
-const port = process.env.PORT || 3000;
-
-const app = express();
+const server = require('./server')
 
 
-app.use(express.static('./public'));
-
-app.get('/api/prices', (req, res)=>{
-    const data = require('./public/api/prices.json')
-    res.json(data);
-});
-
-app.get('/api/staff', (req, res)=>{
-    const data = require('./public/api/staff.json')
-    res.json(data);
-})
-
-app.get('/api/staff/:name', (req, res)=>{
-    const data = require(`./public/api/${req.params.name}.json`)
-    res.json(data);
-})
-
-app.listen(port);
+let myServer = new server.Server(process.env.PORT || 3000);
+myServer.startServer();
