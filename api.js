8addcc3d16e2a980560a245417a48977f13b517c
Anthony <amdecapio@gmail.com>
2020-12-09 20:07:18 -0500

    Started Backend Auth

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const path = require('path')
 
 
 class API{
@@ -10,21 +10,31 @@ class API{
 
 
     startAPI(){
-        
+        //Public API Calls
         this.app.get('/api/prices', (req, res)=>{
-            const data = require('../public/api/prices.json')
+            const data = require('./public/api/prices.json')
             res.json(data);
         });
 
         this.app.get('/api/staff', (req, res)=>{
-            const data = require('../public/api/staff.json')
+            const data = require('./public/api/staff.json')
             res.json(data);
         });
-
+        
         this.app.get('/api/staff/:name', (req, res)=>{
             const data = require(`./public/api/${req.params.name}.json`)
             res.json(data);
         });
+        
+        //Auth Calls
+        this.app.get('/auth/admin/:token', (req, res)=>{
+            if (req.params.token == "test"){
+                res.redirect('../../admin/')
+            }
+            else{
+                res.sendStatus(403)
+            }
+        })
 
     }
 
